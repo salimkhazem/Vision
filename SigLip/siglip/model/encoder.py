21c96c734ce300cb47696ed2f51bd3e95bb95c5e
Salim Khazem <salim.khazem@centralesupelec.fr>
2025-11-03 01:02:10 +0100

    [Siglip] Adding Encoder

---
+++ SigLip/siglip/model/encoder.py
@@ -0,0 +1,29 @@
+import torch 
+import torch.nn as nn
+
+from config_siglip import SiglipConfig
+from encoder_layer import SiglipEncoderLayer 
+
+class SiglipEncoder(nn.Module): 
+    def __init__(self, config): 
+        super().__init__() 
+        self.layers = nn.ModuleList( 
+            [
+                SiglipEncoderLayer(config) 
+                for _ in range(config.num_hidden_layers) 
+            ]
+        ) 
+
+    def forward(self, inputs_embeds: torch.Tensor) -> torch.Tensor: 
+        hidden_states = inputs_embeds 
+        for encoder_layer in self.layers: 
+            hidden_states = encoder_layer(hidden_states)
+        return hidden_states  
+
+
+if __name__ == "__main__": 
+    x = torch.rand(1, 196, 768)
+    config = SiglipConfig() 
+    encoder = SiglipEncoder(config)
+    out = encoder(x) 
+    print(f"Input: {x.shape} -> Output: {out.shape}")
