3ec02ac69cbd1d5f3ccdab8ba2414d845b245011
Salim Khazem <salim.khazem@centralesupelec.fr>
2025-11-03 01:01:50 +0100

    [Siglip] Adding config

---
+++ SigLip/siglip/model/config_siglip.py
@@ -0,0 +1,10 @@
+
+class SiglipConfig:
+    hidden_size: int = 768
+    num_attention_heads: int = 12
+    intermediate_size: int = 3072
+    layer_norm_eps: float = 1e-6
+    num_hidden_layers: int = 12
+    image_size: int = 224
+    patch_size: int = 16
+    num_channels: int = 3
